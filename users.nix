2ba4c8d6a66a9228ed3e2fb24aa62bf473aacd1b
cethien <borislaw.sotnikow@kuhfussonline.com>
2024-10-22 18:33:03 +0200

    chore: reorganized and added users

--- users.nix
+++ users.nix
@@ -1,34 +1,48 @@
 { config, pkgs, ... }:
 
 {
-  users.users.ansible = {
-    isNormalUser = true;
-    description = "ansible@kuhfuss.local";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCriVrxmwrYX2r6gbGfiQC8P8uTueLpmtL8MM9nA+3l ansible@kuhfuss.local"
-    ];
-    packages = with pkgs; [];
-  };
+  users.users = {
 
-  users.users.borislaw_sotnikow = {
-    isNormalUser = true;
-    description = "borislaw.sotnikow@kuhfussonline.com";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWejPaZihz64Fk2/NUbmA6MbNLQz+AGDgy2UXLfe0ci borislaw.sotnikow@kuhfussonline.com"
-    ];
-    packages = with pkgs; [];
-  };
+    ansible = {
+      isNormalUser = true;
+      description = "ansible@kuhfuss.local";
+      extraGroups = [ "networkmanager" "wheel" "docker" ];
 
-  users.users.sven_giesecke = {
-    isNormalUser = true;
-    description = "sven.giesecke@kuhfussonline.com";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVA1+xsucJUXKpbe+aLoNNhUV1XRK5dykfEGfuDZcHg sven.giesecke@kuhfussonline.com"
-    ];
-    packages = with pkgs; [];
-  };
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCriVrxmwrYX2r6gbGfiQC8P8uTueLpmtL8MM9nA+3l ansible@kuhfuss.local"
+      ];
+      packages = with pkgs; [];
+    };
 
+    sven_giesecke = {
+      isNormalUser = true;
+      description = "sven.giesecke@kuhfussonline.com";
+      extraGroups = [ "networkmanager" "wheel" "docker" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVA1+xsucJUXKpbe+aLoNNhUV1XRK5dykfEGfuDZcHg sven.giesecke@kuhfussonline.com"
+      ];
+      packages = with pkgs; [];
+    };
+
+    borislaw_sotnikow = {
+      isNormalUser = true;
+      description = "borislaw.sotnikow@kuhfussonline.com";
+      extraGroups = [ "networkmanager" "wheel" "docker" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGWejPaZihz64Fk2/NUbmA6MbNLQz+AGDgy2UXLfe0ci borislaw.sotnikow@kuhfussonline.com"
+      ];
+      packages = with pkgs; [];
+    };
+
+    hennes_tarrach-fucke = {
+      isNormalUser = true;
+      description = "hennes.tarrach-fucke@kuhfussonline.com";
+      extraGroups = [ "networkmanager" "wheel" "docker" ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPwy3kXvkpyFpkSr+0W73LYBjXdXkn0E4WTCBbH60Jyu hennes.tarrach-fucke@kuhfussonline.com"
+      ];
+      packages = with pkgs; [];
+    };
+
+  };
 }
