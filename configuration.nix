db339ac8aa561b01f01c65598a3afff90a0c6864
cethien <borislaw.sotnikow@kuhfussonline.com>
2024-11-04 17:29:48 +0100

    fix: removed config oopsie

--- configuration.nix
+++ configuration.nix
@@ -82,10 +82,3 @@
   system.stateVersion = "23.11"; # Did you read the comment?
 
 }
-
-{
-  fileSystems."/mnt/docker-storage" = {
-    device = "200.0.1.49:/lab.datastore";
-    fsType = "nfs";
-  };
-}
