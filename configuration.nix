8dacd6f648096738e649547de921c27c8d123a47
cethien <borislaw.sotnikow@kuhfussonline.com>
2024-11-21 16:37:48 +0100

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
   imports =
     [
       # Include the results of the hardware scan.
@@ -13,14 +23,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
     LC_IDENTIFICATION = "de_DE.UTF-8";
@@ -33,53 +41,36 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Configure keymap in X11
   services.xserver = {
     layout = "de";
     xkbVariant = "nodeadkeys";
     videoDrivers = [ "vmware" ];
   };
 
-  # Configure console keymap
   console.keyMap = "de-latin1-nodeadkeys";
 
   virtualisation.vmware.guest.enable = true;
-
   virtualisation.docker = {
     enable = true;
     liveRestore = false;
   };
 
-  services.prometheus.exporters.node = {
-    enable = true;
-    enabledCollectors = [ "systemd" ];
-    extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" ];
+  services = {
+    openssh.enable = true;
+
+    prometheus.exporters.node = {
+      enable = true;
+      enabledCollectors = [ "systemd" ];
+      extraFlags = [ "--collector.ethtool" "--collector.softirqs" "--collector.tcpstat" ];
+    };
+
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim
     tmux
     python3
-    python312Packages.docker
     git
     htop
   ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-
 }
